4e011286ae04e5a4098e312973fb5b120ec55846
unknown <dmckee763@gmail.com>
2020-10-07 21:27:53 +0100

    Started adding accordion to Collection Overview

---
+++ StarWarsTradingCardCollectionAPI/StarWarsTradingCardCollectionMVC/wwwroot/js/Accordion.js
@@ -0,0 +1,16 @@
+﻿var accordions = document.getElementsByClassName("accordion");
+
+for (var i = 0; i < accordions.length; i++) {
+    accordions[i].onclick = function () {
+        this.classList.toggle('is-open');
+        var content = this.nextElementSibling;
+
+        if (content.style.maxHeight) {
+            // Accordion Open
+            content.style.maxHeight = null;
+        } else {
+            // Accordion Closed
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    }
+}
